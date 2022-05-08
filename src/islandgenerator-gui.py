bfd5379db3848bafe609399edde5c2e05a6c0e37
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-05-08 16:05:14 +0200

    Change GTK => Tkinter

---
+++ src/islandgenerator-gui.py
@@ -0,0 +1,16 @@
+from islandgenerator-algo import generate as generate
+from tkinter import *
+window=Tk()
+# Add widgets here
+
+btn=Button(window, text="Generate")
+btn.place(x=80, y=100)
+
+class importgenerator:
+    btn.bind('<Button-1>', generate)
+
+
+# Set Window
+window.title('Island Generator')
+window.geometry("300x200+10+20")
+window.mainloop()
